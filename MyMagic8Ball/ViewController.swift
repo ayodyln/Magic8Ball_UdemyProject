6f3b7c99527c21220c08e3a82e1a0c9fcab49bd5
Dylan Smith <78442693+ayodyln@users.noreply.github.com>
2022-06-13 21:04:56 -0600

    Updated comment

--- MyMagic8Ball/ViewController.swift
+++ MyMagic8Ball/ViewController.swift
@@ -20,7 +20,6 @@ class ViewController: UIViewController {
         print("App Loaded...")
     }
     
-    //Completed task
     @IBAction func askQuestionButton(_ sender: Any) {
         print("Question being asked...")
         magicBall.image = magicBallArr.randomElement()
